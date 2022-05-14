3f9048278f4cf7ba8b77262e0865ae7f841a5124
Cheng® <cheng106.andy@gmail.com>
2022-05-14 23:43:27 +0800

    chore: move vue file

---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,183 @@
+<template>
+  <el-container style="min-height: 100vh;">
+    <el-aside :width="sideWidth" style="background-color: rgb(238, 241, 246); box-shadow: 2px 0 6px rgb(0 21 41 / 35%)">
+      <el-menu :default-openeds="['1', '3']" style="min-height: 100%; overflow-x: hidden"
+               background-color="rgb(48,65,86)"
+               text-color="#fff"
+               active-text-color="#ffd04b"
+               :collapse-transition="false"
+               :collapse="isCollapse"
+      >
+        <div style="height: 60px;line-height: 60px; text-align: center">
+          <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; margin-right: 5px;">
+          <b style="color: #42b983" v-show="logoTextShow">後台管理系統</b>
+        </div>
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-message"></i>
+            <span>選單一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title"><i class="el-icon-menu"></i>
+            <span>選單二</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="2-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title"><i class="el-icon-setting"></i>
+            <span>選單三</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="3-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="3-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+
+    <el-container>
+      <el-header style="font-size: 12px; border-bottom: 1px solid #ccc; line-height: 60px; display: flex">
+        <div style="flex: 1; font-size: 25px">
+          <span :class="collapseBtnClass" style="cursor:pointer" @click="collapse"></span>
+        </div>
+        <el-dropdown style="width: 70px; cursor:pointer">
+          <i class="el-icon-arrow-down" style="margin-left: 5px">
+            <span>王小虎</span>
+          </i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>個人資訊</el-dropdown-item>
+            <el-dropdown-item>登出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+
+      <el-main>
+
+        <div style="margin-bottom: 30px">
+          <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{path:'/'}">首頁</el-breadcrumb-item>
+            <el-breadcrumb-item>使用者管理</el-breadcrumb-item>
+          </el-breadcrumb>
+        </div>
+
+        <div style="padding: 10px 0">
+          <el-input style="width: 200px" placeholder="請輸入名稱" suffix-icon="el-icon-search" class="ml-5"></el-input>
+          <el-input style="width: 200px" placeholder="請輸入Email" suffix-icon="el-icon-message" class="ml-5"></el-input>
+          <el-input style="width: 200px" placeholder="請輸入地址" suffix-icon="el-icon-position" class="ml-5"></el-input>
+          <el-button class="ml-5" type="primary">搜尋</el-button>
+        </div>
+
+        <div style="margin: 10px 0">
+          <el-button type="primary">新增 <i class="el-icon-circle-plus-outline"></i></el-button>
+          <el-button type="primary">匯入 <i class="el-icon-bottom"></i></el-button>
+          <el-button type="primary">匯出 <i class="el-icon-top"></i></el-button>
+          <el-button type="danger">批次刪除 <i class="el-icon-remove-outline"></i></el-button>
+        </div>
+
+        <el-table :data="tableData" border stripe :header-cell-class-name="headerBg">
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址">
+          </el-table-column>
+          <el-table-column>
+            <template slot-scope="scope">
+              <el-button type="success">編輯 <i class="el-icon-edit"></i></el-button>
+              <el-button type="danger">刪除 <i class="el-icon-remove-outline"></i></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <div style="padding: 10px 0">
+          <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="currentPage4"
+              :page-sizes="[5, 10, 15, 20]"
+              :page-size="10"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="400">
+          </el-pagination>
+        </div>
+
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+
+  data() {
+    const item = {
+      date: '2022-05-02',
+      name: '小名',
+      address: '台北市民權西路213巷'
+    };
+    return {
+      tableData: Array(10).fill(item),
+      msg: "Hello World",
+      collapseBtnClass: 'el-icon-s-fold',
+      isCollapse: false,
+      sideWidth: 200,
+      logoTextShow: true,
+      headerBg: 'headerBg'
+    }
+  },
+  methods: {
+    // 點收縮按鈕觸發
+    collapse() {
+      this.isCollapse = !this.isCollapse;
+      if (this.isCollapse) {
+        this.sideWidth = 64
+        this.collapseBtnClass = 'el-icon-s-unfold'
+        this.logoTextShow = false
+      } else {
+        this.sideWidth = 200
+        this.collapseBtnClass = 'el-icon-s-fold'
+        this.logoTextShow = true
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.headerBg {
+  background: rgba(203, 202, 202, 0.93) !important;
+}
+</style>
